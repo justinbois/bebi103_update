180dc4d18faa7daf13eb73e8b4ef939bb2768a9f
justinbois <bois@caltech.edu>
2020-02-06 17:42:03 -0800

    Added ant traffic data.

--- run_update.sh
+++ run_update.sh
@@ -2,3 +2,4 @@ pip install --upgrade bebi103;
 pip install --upgrade bokeh-catplot;
 wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/rasson_dissociation_anisotropy.csv;
 wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/rasson_competition_anisotropy.csv;
+wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/ant_traffic.txt;
