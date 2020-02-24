be325b2f38ba5d3ec3a91d3bca05070f8ba5ab6a
Justin Bois <bois@caltech.edu>
2020-02-23 23:49:24 -0800

    Added Elowitz and Caulobacter data.

--- run_update.sh
+++ run_update.sh
@@ -8,3 +8,5 @@ pip install --upgrade bokeh-catplot;
 wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/rasson_dissociation_anisotropy.csv;
 wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/rasson_competition_anisotropy.csv;
 wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/ant_traffic.txt;
+wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/elowitz_et_al_2002_fig_3a.csv;
+wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/hw6.1_caulobacter_growth_events.csv;
