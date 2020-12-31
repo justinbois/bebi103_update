a1b54b9aa1e3b4d83672469e306fa2ec98aec526
justinbois <bois@caltech.edu>
2020-12-30 22:00:24 -0800

    No updates needed now.

--- run_update.sh
+++ run_update.sh
@@ -1,12 +1,4 @@
 sudo yum -y update;
-conda update -y --all;
-jupyter labextension install --no-build @ijmbarr/jupyterlab_spellchecker;
-jupyter labextension install --no-build @pyviz/jupyterlab_pyviz;
-jupyter lab build;
-pip install --upgrade bebi103;
-pip install --upgrade iqplot;
-wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/rasson_dissociation_anisotropy.csv;
-wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/rasson_competition_anisotropy.csv;
-wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/ant_traffic.txt;
-wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/elowitz_et_al_2002_fig_3a.csv;
-wget -P ~/data/ https://s3.amazonaws.com/bebi103.caltech.edu/data/hw6.1_caulobacter_growth_events.csv;
+# conda update -y --all;
+# pip install --upgrade bebi103;
+# pip install --upgrade iqplot;
