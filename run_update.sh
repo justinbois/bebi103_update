8aa5a0cc480f7629181a0454c8af4e39ef600aec
justinbois <bois@caltech.edu>
2021-03-01 07:41:54 -0800

    Update to include new data sets and put ArviZ at v. 0.11.1

--- run_update.sh
+++ run_update.sh
@@ -4,8 +4,8 @@ sudo yum -y update;
 # Update Python packages
 conda update -y --all;
 
-# Package updates
-pip install --upgrage arviz cmdstanpy;
+# Package updates; keep ArviZ at 0.11.1
+pip install --upgrage arviz==0.11.1 cmdstanpy;
 pip install --upgrade bebi103;
 
 # Update cmdstan
@@ -14,3 +14,9 @@ python -c "import cmdstanpy; cmdstanpy.install_cmdstan()";
 # Get new data sets
 wget https://s3.amazonaws.com/bebi103.caltech.edu/data/naganathan_explanted_somite_deformability.csv;
 mv naganathan_explanted_somite_anisotropy.csv $HOME/data/;
+
+wget https://s3.amazonaws.com/bebi103.caltech.edu/data/wolfenden_arrhenius.csv;
+mv wolfenden_arrhenius.csv $HOME/data/;
+
+wget https://s3.amazonaws.com/bebi103.caltech.edu/data/roeschinger_growth_rate_data.csv;
+mv roeschinger_growth_rate_data.csv $HOME/data/;
