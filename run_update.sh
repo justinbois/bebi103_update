1d59d60beab214e4783b756740d59a95aefc3954
Justin Bois <bois@caltech.edu>
2024-01-02 15:44:08 -0800

    Tweak for 2024

--- run_update.sh
+++ run_update.sh
@@ -4,12 +4,12 @@ sudo yum -y update;
 # Update Python packages
 # conda update -y --all;
 
-# Package updates; keep ArviZ at 0.11.1
+# Update cmdstan
+python -c "import cmdstanpy; cmdstanpy.install_cmdstan()";
+
+# JB's package updates
 pip install --upgrade bebi103;
 pip install --upgrade iqplot;
-
-# Update cmdstan
-# python -c "import cmdstanpy; cmdstanpy.install_cmdstan()";
 
 # Get new data sets
 # wget https://s3.amazonaws.com/bebi103.caltech.edu/data/naganathan_explanted_somite_deformability.csv;
